899a38d6b09bbec2dcc934837a0a9ad66d20d88d
Lsoe23 <Lahpwesoe@gmail.com>
2024-07-16 18:34:48 -0500

    Creating class
    class Customer:
        pass

---
+++ BankProject.py
@@ -0,0 +1,15 @@
+#Creating Customer Class with 4 parameters
+class Customer:
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+    
+
+
+#Create a dictionary of existing customers 
+#Add two existing customers
+
+
+#Sign Up
